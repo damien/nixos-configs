08228f9173c2164f6299e0268562b8be4a00fd7f
Damien Wilson <damien@absurd.engineering>
2025-02-07 15:49:05 -0800

    Minimal WSL configuration for NixOS.

--- configuration.nix
+++ configuration.nix
@@ -11,11 +11,13 @@
   imports = [
     # include NixOS-WSL modules
     <nixos-wsl/modules>
+    # VSCode Server for WSL
+    (fetchTarball "https://github.com/nix-community/nixos-vscode-server/tarball/master")
   ];
 
   wsl.enable = true;
   wsl.defaultUser = "nixos";
-
+  
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -24,4 +26,14 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  environment.systemPackages = with pkgs; [
+    git
+    neovim
+    wget
+  ];
+
+  services.vscode-server.enable = true;
 }
